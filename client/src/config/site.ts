29d3c40cb64b20dd3c7e4ddaa36d4fff22dff4f3
prounknown055-pixel <prounknown055@gmail.com>
2026-01-21 19:21:39 +0530

    Create site.ts

---
+++ client/src/config/site.ts
@@ -0,0 +1,18 @@
+export const SITE = {
+  whatsapp: "919876543210",
+
+  social: {
+    instagram: "https://instagram.com/YOURPAGE",
+    facebook: "https://facebook.com/YOURPAGE",
+    youtube: "https://youtube.com/@YOURCHANNEL",
+    pinterest: "https://pinterest.com/YOURPAGE",
+    telegram: "https://t.me/YOURCHANNEL",
+  },
+
+  images: {
+    hero:
+      "https://images.unsplash.com/photo-1616091093729-c0463d3957eb",
+    about:
+      "https://images.unsplash.com/photo-1551024506-0bccd828d307",
+  },
+};
