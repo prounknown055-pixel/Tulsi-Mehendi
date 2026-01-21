f32f3fc864530fa1054cfb2721db32fd970b27d3
prounknown055-pixel <prounknown055@gmail.com>
2026-01-21 18:25:16 +0530

    Create siteData.js

---
+++ client/src/lib/siteData.js
@@ -0,0 +1,15 @@
+export const siteData = {
+  brandName: "Tulsi Mehendi",
+
+  whatsapp: "919999999999",   // apna number baad me change
+  instagram: "https://instagram.com/tulsimehendi",
+  facebook: "https://facebook.com/tulsimehendi",
+  pinterest: "https://pinterest.com/tulsimehendi",
+
+  gallery: [
+    "/images/mehendi1.jpg",
+    "/images/mehendi2.jpg",
+    "/images/mehendi3.jpg",
+    "/images/mehendi4.jpg"
+  ]
+};
